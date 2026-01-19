57d4e04d6ccc5362de1c3dfe7e6c0cc913507448
Daniels <A230637DP@rvt.lv>
2026-01-19 21:17:26 +0200

    Pievienots Vuetify 3.

---
+++ frontend/vue-project/src/views/home.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <v-card class="pa-4">
+      <v-card-title>Home Page</v-card-title>
+      <v-card-text>
+        This is the home page.
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
